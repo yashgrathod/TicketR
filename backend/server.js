5dd2dae4fe63a49e4afa2e67cc6605481042da14
yashgrathod <yashgrathod19@gmail.com>
2026-06-08 22:53:39 +0530

    Final version

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const ticketRoutes = require('./routes/tickets');
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 const apiLimiter = rateLimit({
